f4b63aa6b19f273fa20f8c9a430b60052abdf4e3
Thien Vo <vanthienpc@gmail.com>
2021-07-13 12:17:55 +0700

    remove variable unused

--- src/views/components/SearchBox/index.tsx
+++ src/views/components/SearchBox/index.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { Input } from 'antd';
 import { Dispatch } from 'redux';
 import styled from 'styled-components';
-import IStore from 'models/StoreModel';
-import { useDispatch, useSelector } from 'utilities/HookUtility';
-import { selectRequest } from 'selectors/RequestSelector';
+import { useDispatch } from 'utilities/HookUtility';
 import * as MusicVideoAction from 'stores/musicVideo/MusicVideoAction';
-import * as MusicVideoType from 'stores/musicVideo/MusicVideoType';
 import SearchIcon from 'assets/search.svg';
 
 const SearchField = styled(Input).attrs(() => ({
@@ -17,10 +14,6 @@ const SearchField = styled(Input).attrs(() => ({
 
 const SearchBox: React.FC = () => {
   const dispatch: Dispatch = useDispatch();
-
-  const isRequest: boolean = useSelector((state: IStore) =>
-    selectRequest(state, [MusicVideoType.FETCH_MUSIC_VIDEO_REQUEST]),
-  );
 
   const onSearch: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
     const { value } = e.currentTarget;
